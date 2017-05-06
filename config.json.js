337a79626f0a93eedcf6d50c1c06a8fc43ebcf19
Kenney <zql254@vip.qq.com>
2017-05-06 10:43:23 +0800

    add into softcenter

---
+++ config.json.js
@@ -0,0 +1,8 @@
+{
+    "description": "上下行带宽提速", 
+    "home_url": "Module_kuainiao.asp", 
+    "md5": "33b0950350440449ec6c03e9aceb6c3b", 
+    "module": "kuainiao", 
+    "title": "kuainiao", 
+    "version": "1.0.3"
+}
